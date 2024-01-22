cd7f4bf182f48a3220c5cb47ddab51dd61609898
UsefulTornado <Azarel.url@gmail.com>
2024-01-23 01:20:59 +0300

    refactoring

--- src/cky_parser.py
+++ src/cky_parser.py
@@ -2,25 +2,24 @@ from .cfg import Terminal, CFGrammar
 
 
 class CKYParser:
-    def __init__(self, grammar):
+    def __init__(self, grammar: CFGrammar):
         self.grammar = grammar
 
-    def accepts(self, word):
-        chars = list(word)
+    def accepts(self, word: str) -> bool:
         grammar = CFGrammar.to_chomsky_normal_form(self.grammar)
         nonterminals = [grammar.start] + list(grammar.nonterminals - {grammar.start})
         table = [
-            [[False for _ in range(len(nonterminals))] for _ in range(len(chars))]
-            for _ in range(len(chars))
+            [[False for _ in range(len(nonterminals))] for _ in range(len(word))]
+            for _ in range(len(word))
         ]
 
-        for char_idx, char in enumerate(chars):
+        for char_idx, char in enumerate(word):
             for rule in grammar.rules:
                 if len(rule.right) == 1 and rule.right[0] == Terminal(char):
                     table[0][char_idx][nonterminals.index(rule.left)] = True
 
-        for l in range(1, len(chars)):
-            for s in range(len(chars) - l):
+        for l in range(1, len(word)):
+            for s in range(len(word) - l):
                 for p in range(l):
                     for rule in grammar.rules:
                         if len(rule.right) == 2:
@@ -32,4 +31,4 @@ class CKYParser:
                             ):
                                 table[l][s][nonterminals.index(rule.left)] = True
 
-        return table[len(chars) - 1][0][0]
+        return table[len(word) - 1][0][0]
